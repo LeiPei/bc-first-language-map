bedcd87e5c38e82f997b1fd9439c42875501f35b
Lei Pei <hileopei@gmail.com>
2019-05-20 22:26:49 -0700

    added fake marker and polygon

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script>
+const POINTS = [
+  { lat: 48.441883, lng: -124.145295 },
+  { lat: 48.309815, lng: -123.565201 },
+  { lat: 48.313350, lng: -123.676739 },
+  { lat: 48.454504, lng: -122.928034 },
+  { lat: 49.246292, lng: -123.116226 },
+  { lat: 49.056801, lng: -122.285042 }
+]
 export default {
   mounted () {
     // set height first
@@ -13,7 +21,14 @@ export default {
 
     let mymap = L.map('mapid', { //eslint-disable-line
       zoomControl: false
-    }).setView([49.246292, -123.116226], 13)
+    }).setView([49.246292, -123.116226], 8) // 13
+
+    // eslint-disable-next-line
+    let myIcon = L.icon({
+      iconUrl: '../../assets/map-pin-silhouette.png',
+      iconSize: [38, 95]
+    })
+
     // add zoom control with your options
     L.control.zoom({ //eslint-disable-line
       position: 'topright'
@@ -25,6 +40,19 @@ export default {
       id: 'mapbox.streets',
       accessToken: 'pk.eyJ1IjoibHBlaSIsImEiOiJjanZ2a2NxdGIwem4yM3pwMWFmdDBsYzQ2In0.N7-edRWRWJ5f33kGyB9SNw'
     }).addTo(mymap)
+
+    // process the points
+    POINTS.forEach(p => {
+      L.marker([p.lat, p.lng]).addTo(mymap) //eslint-disable-line
+    })
+
+    // add polygon
+    // eslint-disable-next-line
+    L.polygon([
+      [49.36672, -123.16652],
+      [49.074329, -122.559319],
+      [49.09, -123.133568]
+    ], { color: '#521170' }).addTo(mymap)
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -20,6 +20,15 @@ window.$ = window.jQuery = require('jquery')
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 
+// eslint-disable-next-line
+delete L.Icon.Default.prototype._getIconUrl
+// eslint-disable-next-line
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
+
 new Vue({
   router,
   store,
